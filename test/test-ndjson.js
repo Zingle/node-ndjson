719091f5c0c2f905f718c1df9b75e860bb84dc60
Richard Remer <rich.remer@gmail.com>
2021-02-16 11:28:50 -0800

    strict mode option

--- test/test-ndjson.js
+++ test/test-ndjson.js
@@ -2,46 +2,70 @@ const expect = require("expect.js");
 const sinon = require("sinon");
 const ndjson = require("..");
 
-const EOL = Object.freeze([10]);    // end-of-line byte sequence
+const EOL = Buffer.from([10]);          // end-of-line byte sequence
 
 describe("ndjson({strict})", () => {
-    const strict = true;
     let stream;
 
-    beforeEach(() => {
-        stream = ndjson({strict});
+    describe("-> Transform", () => {
+        beforeEach(() => {
+            stream = ndjson();
+        });
+
+        it("should return Transform stream", () => {
+            expect(stream.readable).to.be(true);
+            expect(stream.writable).to.be(true);
+        });
+
+        it("should output parsed JSON", () => {
+            const bodyA = JSON.stringify({A:"foo"});
+            const bodyB = JSON.stringify({B:"foo"});
+
+            stream.write(Buffer.from(bodyA)); stream.write(EOL);
+            stream.write(Buffer.from(bodyB)); stream.write(EOL);
+            stream.end();
+
+            const docA = stream.read();
+            const docB = stream.read();
+
+            expect(docA).to.be.an("object");
+            expect(docB).to.be.an("object");
+            expect(docA.A).to.be("foo");
+            expect(docB.B).to.be("foo");
+        });
     });
 
-    it("should return Transform stream", () => {
-        expect(stream.readable).to.be(true);
-        expect(stream.writable).to.be(true);
+    describe("w/ strict mode enabled", () => {
+        beforeEach(() => {
+            stream = ndjson({strict: true});
+        });
+
+        it("should error on missing terminator", () => {
+            const body = JSON.stringify({A:"foo"});
+            const errspy = sinon.spy();
+
+            stream.on("error", errspy);
+            stream.write(Buffer.from(body));
+            stream.end();
+
+            expect(errspy.called).to.be(true);
+        });
     });
 
-    it("should output parsed JSON", () => {
-        const bodyA = JSON.stringify({A:"foo"});
-        const bodyB = JSON.stringify({B:"foo"});
+    describe("w/ strict mode disabled", () => {
+        beforeEach(() => {
+            stream = ndjson({strict: false});
+        });
 
-        stream.write(Buffer.from(bodyA)); stream.write(Buffer.from(EOL));
-        stream.write(Buffer.from(bodyB)); stream.write(Buffer.from(EOL));
-        stream.end();
+        it("should handle last line", () => {
+            const body = JSON.stringify({A:"foo"});
 
-        const docA = stream.read();
-        const docB = stream.read();
+            stream.write(Buffer.from(body));
+            stream.end();
 
-        expect(docA).to.be.an("object");
-        expect(docB).to.be.an("object");
-        expect(docA.A).to.be("foo");
-        expect(docB.B).to.be("foo");
-    });
+            const doc = stream.read();
 
-    it("should error on missing terminator if strict", () => {
-        const body = JSON.stringify({A:"foo"});
-        const errspy = sinon.spy();
-
-        stream.on("error", errspy);
-        stream.write(Buffer.from(body));
-        stream.end();
-
-        expect(errspy.called).to.be(true);
+            expect(doc.A).to.be("foo");
+        });
     });
 });
